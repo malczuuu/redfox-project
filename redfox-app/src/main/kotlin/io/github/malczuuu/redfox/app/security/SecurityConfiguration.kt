3766b8d1c6c75decdc9eda47226c4b3d1c5c18c1
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-03-30 00:17:27 +0200

    Add initial spring security settings

---
+++ redfox-app/src/main/kotlin/io/github/malczuuu/redfox/app/security/SecurityConfiguration.kt
@@ -0,0 +1,93 @@
+package io.github.malczuuu.redfox.app.security
+
+import io.github.problem4j.core.Problem
+import io.github.problem4j.core.ProblemBuilder
+import io.github.problem4j.core.ProblemContext
+import io.github.problem4j.spring.webmvc.resolver.NoHandlerFoundProblemResolver
+import io.github.problem4j.spring.webmvc.resolver.NoResourceFoundProblemResolver
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import tools.jackson.databind.json.JsonMapper
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfiguration(jsonMapper: JsonMapper) {
+
+  private val problemString401 =
+      jsonMapper.writeValueAsString(Problem.of(HttpStatus.UNAUTHORIZED.value()))
+
+  @Bean
+  fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    http {
+      csrf { disable() }
+      sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
+      authorizeHttpRequests {
+        authorize("/api/v1/**", permitAll)
+        authorize("/actuator/**", permitAll)
+        authorize("/**", denyAll)
+      }
+      exceptionHandling {
+        authenticationEntryPoint = { _, response, _ -> write401(response) }
+        accessDeniedHandler = { _, response, _ -> write401(response) }
+      }
+    }
+    return http.build()
+  }
+
+  @Bean
+  fun passwordEncoder(): PasswordEncoder {
+    return BCryptPasswordEncoder()
+  }
+
+  @Bean
+  fun noHandlerFoundProblemResolver(): NoHandlerFoundProblemResolver {
+    return ExtNoHandlerFoundProblemResolver()
+  }
+
+  @Bean
+  fun noResourceFoundProblemResolver(): NoResourceFoundProblemResolver {
+    return ExtNoResourceFoundProblemResolver()
+  }
+
+  private fun write401(response: HttpServletResponse) {
+    response.status = HttpStatus.UNAUTHORIZED.value()
+    response.contentType = MediaType.APPLICATION_PROBLEM_JSON_VALUE
+    response.writer.write(problemString401)
+  }
+
+  private open class ExtNoHandlerFoundProblemResolver : NoHandlerFoundProblemResolver() {
+    override fun resolveBuilder(
+        context: ProblemContext,
+        ex: Exception,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+    ): ProblemBuilder {
+      return Problem.builder().status(HttpStatus.UNAUTHORIZED.value())
+    }
+  }
+
+  private open class ExtNoResourceFoundProblemResolver : NoResourceFoundProblemResolver() {
+    override fun resolveBuilder(
+        context: ProblemContext,
+        ex: Exception,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+    ): ProblemBuilder {
+      return Problem.builder().status(HttpStatus.UNAUTHORIZED.value())
+    }
+  }
+}
