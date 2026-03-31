86f2267efe5159e41035c4f192a3338bf0a1fc1a
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-03-31 21:20:28 +0200

    Add initial impl of UserDetailsService

---
+++ redfox-authserver/src/main/kotlin/io/github/malczuuu/redfox/authserver/core/UserDetailsServiceImpl.kt
@@ -0,0 +1,20 @@
+package io.github.malczuuu.redfox.authserver.core
+
+import io.github.malczuuu.redfox.authserver.domain.UserRepository
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UserDetailsServiceImpl(private val userRepository: UserRepository) : UserDetailsService {
+
+  override fun loadUserByUsername(username: String): UserDetails {
+    val user =
+        userRepository.findByLogin(username).orElseThrow {
+          UsernameNotFoundException.fromUsername(username)
+        }
+    return UserPrincipal(user.login, user.passhash, listOf(SimpleGrantedAuthority("ROLE_USER")))
+  }
+}
