4910f02e11da1348f931df66a82a50380a713957
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-04-02 19:44:08 +0200

    Spin

--- redfox-webapp/src/app/pages/oauth2-callback/oauth2-callback.ts
+++ redfox-webapp/src/app/pages/oauth2-callback/oauth2-callback.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-oauth2-callback',
-  template: '<p>Signing in...</p>',
+  imports: [MatProgressSpinnerModule],
+  template: '<div class="callback-container"><mat-spinner diameter="48"></mat-spinner></div>',
+  styles:
+    '.callback-container { display: flex; align-items: center; justify-content: center; height: 100vh; }',
 })
 export class OAuth2Callback implements OnInit {
   constructor(
